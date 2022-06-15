7e14caf6a471a34fed543cf9238337da6ab8f1f4
yurui <ttttttesla0214@gmail.com>
2022-06-16 00:58:47 +0800

    main方法增加空格

--- rmq-sb/src/main/java/com/rmq/RmqSbApplication.java
+++ rmq-sb/src/main/java/com/rmq/RmqSbApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RmqSbApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RmqSbApplication.class, args);
     }
 
